a0e950932342bf6b0b0211dfe05b7380be10e555
monik <monik@LAPTOP-GPEQ7QFB>
2018-02-02 18:28:25 -0500

    First Commit

---
+++ src/org/aspect/LoggingAspect.java
@@ -0,0 +1,34 @@
+package org.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+
+public class LoggingAspect {
+	
+	
+	public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+		
+		Object returnValue = null;
+		
+		try {
+			System.out.println("Before Advice");
+			returnValue = proceedingJoinPoint.proceed();
+			System.out.println("After Returning");
+			
+		} catch (Throwable e) {
+			System.out.println("After Throwing");		
+		}
+		
+		System.out.println("After Finally");
+		return returnValue;
+	    
+	}
+	
+		
+	public void loggingAdvice() {
+		System.out.println("Logging from the advice");
+	}
+	
+	
+	
+}
